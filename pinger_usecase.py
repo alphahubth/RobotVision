55af4c5d4f0f5565e3cdf99c64ae3c9775c7b36c
Elstargo00 <a.Thaprasop.J@icloud.com>
2024-01-09 11:49:46 +0700

    updates

---
+++ pinger_usecase.py
@@ -0,0 +1,22 @@
+
+from multiprocessing import Queue, Process
+
+
+
+def main():
+
+    camera_processes = [
+        Process(target=)
+    ]
+
+
+
+if __name__ == "__main__":
+
+
+
+    command_queues = [Queue() for _ in range(3)]
+    result_queue = Queue()
+
+
+    main()
